bc91bf0a7c4835d2898168dc9b276fd4227d0552
YS-0911 <a9187049@gmail.com>
2025-09-16 11:00:22 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// https://study-website-be-bbb1539aa813.herokuapp.com/ <= 이걸로 주소 바꿔야함
+// api문서 확인하기: https://hackmd.io/@oW_dDxdsRoSpl0M64Tfg2g/B1D0H-JnT
+const API_KEY = 'eb7d6a7c7ea74b33a9b317988f77e6b6';
+
+const getLatestNews = ()=>{
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  // URL 인스턴스는 JS에서 필요한 함수와 변수들을 제공
+  console.log(url);
+  const response = fetch(url);
+}
+getLatestNews();
